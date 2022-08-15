ca0875fd494f902cdc5edf8de4eaddea2aa24c33
Dany D <55385221+FoobarIT@users.noreply.github.com>
2022-08-15 12:45:35 +0200

    finaly

--- js/github.api.js
+++ js/github.api.js
@@ -2,8 +2,13 @@
     async function getUser() {
         try {
           const response = await fetch('https://api.github.com/users/foobarit');
-            console.log(response);
-          return response.data;
+            const data = await response.json()
+            console.log(data)
+            document.getElementById('followers').innerHTML = data.followers;
+            document.getElementById('following').innerHTML = data.following;
+            document.getElementById('repos').innerHTML = data.public_repos;
+            document.getElementById('gist').innerHTML = data.public_gists;
+          
         } catch (err) {
           console.log(err);
         }
