b64f7c8ac79175afc799db10a75ff7cece0cba96
Dany D <55385221+FoobarIT@users.noreply.github.com>
2022-08-15 11:57:02 +0200

    git api apply

--- js/github.api.js
+++ js/github.api.js
@@ -1,9 +1,8 @@
 axios.get('https://api.github.com/users/foobarit')
     .then(function (response) {
         // handle success
-        console.log(response);
-        $('#my_repos').text(response.data.public_repos)
-        $('#my_follower').text(response.data.followers)
+        console.log(response.json());
+        
     })
     .catch(function (error) {
         // handle error
